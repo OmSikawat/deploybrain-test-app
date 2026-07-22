38116710b33109defd2f39c40b086056a4dfb07c
OmSikawat <omsikawat67@gmail.com>
2026-07-22 14:31:36 +0530

    Test

--- src/test/java/com/testapp/AppTest.java
+++ src/test/java/com/testapp/AppTest.java
@@ -8,13 +8,4 @@ class AppTest {
     void testGreet() {
         assertEquals("Hello World", App.greet("World"));
     }
-
-    @Test
-void testRetryLogic() throws InterruptedException {
-    int attempts = 0;
-    while (attempts >= 0) {  // bug: condition never becomes false
-        Thread.sleep(120000);
-        attempts++;
-    }
-}
 }
