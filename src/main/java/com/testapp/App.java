eab3b13a114f28c786d1b6a3b215baa74a39422f
OmSikawat <omsikawat67@gmail.com>
2026-07-22 13:56:24 +0530

    Test

--- src/main/java/com/testapp/App.java
+++ src/main/java/com/testapp/App.java
@@ -2,6 +2,6 @@ package com.testapp;
 
 public class App {
     public static String greet(String name) {
-        return "Hello " + name  // missing semicolon
+        return "Hello " + name;
     }
 }
