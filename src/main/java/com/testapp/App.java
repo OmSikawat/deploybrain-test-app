db4fe98f1a2b8a8d7f768c3ae79ff2ac993efe59
OmSikawat <omsikawat67@gmail.com>
2026-07-22 13:47:05 +0530

    Test

--- src/main/java/com/testapp/App.java
+++ src/main/java/com/testapp/App.java
@@ -2,6 +2,6 @@ package com.testapp;
 
 public class App {
     public static String greet(String name) {
-        return "Hey there, " + name; // changed wording, breaks existing assertion
+        return "Hello " + name  // missing semicolon
     }
 }
